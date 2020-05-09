0d14c387fb35c09f54dc96b26c72d82899e80fd3
KumHoe Lau <nerojz86@gmail.com>
2020-05-09 22:04:05 +0800

    refact:SeasonDisplay component
    Style the SeasonDisplay component with CSS.
    Refact the component to display proper message.

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,36 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
-const SeasonDisplay = (props) => {
-  console.log(props);
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it is cold",
+    iconName: "snowflake",
+  },
+};
 
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon icon-left massive ${iconName}`} />
+      <h1>{text}</h1>
+      <i className={`icon icon-right massive ${iconName}`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
