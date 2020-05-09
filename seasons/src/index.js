444da6274a284823b776b06ededea2533f3333ba
KumHoe Lau <nerojz86@gmail.com>
2020-05-09 22:14:58 +0800

    feat: Add message props to Spinner
    Create defaultprops to display default message.
    Use props to customize the message.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -20,7 +20,7 @@ class App extends React.Component {
     } else if (this.state.lat && !this.state.errorMessage) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
