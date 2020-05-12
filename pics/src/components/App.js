47f8a57c333a290e70a305d197c7eed71ff8efea
kumhoe.lau <nerojz86@gmail.com>
2020-05-12 21:42:14 +0800

    feat:Integrate axios library
    Use axios library to make network request.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(`App: ${term}`);
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID Gmzwvjwbz980Ubbfw4y5zB_LCNUDQarEEm3S-QYEc_E",
+      },
+    });
+
+    console.log(response);
   }
 
   render() {
