eaa8dcb2fd33b91e81dcedd800ba423469478475
KumHoe Lau <nerojz86@gmail.com>
2020-05-17 21:28:26 +0800

    refact:Refact youtube utils

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import SearchBar from "./SearchBar/SearchBar";
+import youtube from "./apis/youtube";
+
+const KEY = "AIzaSyAthC2h_xMKBwoYFb2hmTv-EbunOMBfWXI";
 
 class App extends React.Component {
+  onTermSubmit = async (term) => {
+    var res = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+
+    console.log(res);
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- videos/src/components/apis/youtube.js
+++ videos/src/components/apis/youtube.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAthC2h_xMKBwoYFb2hmTv-EbunOMBfWXI";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    key: KEY,
-  },
 });
