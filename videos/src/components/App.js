92349f5d07b9b5cb291df4cd0dd8bb19402ea5fc
KumHoe Lau <nerojz86@gmail.com>
2020-05-17 21:41:30 +0800

    feat:Create VideoList and VideoItem functional component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import youtube from "./apis/youtube";
+import VideoList from "./VideoList/VideoList";
 
 const KEY = "AIzaSyAthC2h_xMKBwoYFb2hmTv-EbunOMBfWXI";
 
 class App extends React.Component {
+  state = { videos: [] };
+
   onTermSubmit = async (term) => {
-    var res = await youtube.get("/search", {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
         part: "snippet",
@@ -15,13 +18,14 @@ class App extends React.Component {
       },
     });
 
-    console.log(res);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
